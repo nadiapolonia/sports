0b1acac5c3db9616b939057467a43e884ffc3a18
Nadia Murchison <nadiamurchison@gmail.com>
2019-08-06 23:33:00 -0400

    week2day2 complete

--- public/main.js
+++ public/main.js
@@ -1,47 +1,105 @@
 const main = () => {
-  if (document.querySelector("h1.hello-world")) {
-    document.querySelector("h1.hello-world").textContent = "Hello, World!";
-  }
+  document.querySelector(".team-1-score").textContent = 0;
+  document.querySelector(".team-2-score").textContent = 0;
+  document.querySelector(".team-1-name").textContent = "Team 1";
+  document.querySelector(".team-2-name ").textContent = "Team 2";
+  document.querySelector(".team-1-input").value = "";
+  document.querySelector(".team-2-input").value = "";
+  document.querySelector(".team-1-add-1-button").disabled = false;
+  document.querySelector(".team-1-subtract-1-button").disabled = false;
+  document.querySelector(".team-2-subtract-1-button").disabled = false;
+  document.querySelector(".team-2-add-1-button").disabled = false;
+  document.querySelector(".reset").classList.add("hide");
+  console.log("Game reset!");
 };
 
-//const team1add1button = document.querySelector('.team-1-add-1-button');
-//cost team1score = document.querySelector('.team-1-score').textContent;
-//const addscore = parseInt(team1score) +1;
+let currentScore = 0;
+let currentScore1 = 0;
+let currentScore2 = 0;
+outputMessageTeam1 = document.querySelector(".output-1");
+outputMessageTeam2 = document.querySelector(".output-2");
 
+//Team 1
 let addTeam1Score = () => {
   //function to add 1 to Team 1 Score//
   // get the current score
-  let currentScore = document.querySelector(".team-1-score").textContent;
+  let currentScore1 = document.querySelector(".team-1-score").textContent;
   // add 1
-  currentScore = parseInt(currentScore) + 1;
+  currentScore1 = parseInt(currentScore1) + 1;
   // update the html equal it to the addition function
-  document.querySelector(".team-1-score").textContent = currentScore;
-  console.log("adding one to team one score", currentScore);
-};
+  document.querySelector(".team-1-score").textContent = currentScore1;
+  console.log("adding one to team one score", currentScore1);
 
-let addTeam2Score = () => {
-  let currentScore = document.querySelector(".team-2-score").textContent;
-  currentScore = parseInt(currentScore) + 1;
-  document.querySelector(".team-2-score").textContent = currentScore;
-  console.log("adding one to team two score", currentScore);
+  if (currentScore1 >= 21) {
+    console.log("Team 1 Wins!");
+    outputMessageTeam1.textContent = "You win!";
+    outputMessageTeam2.textContent = "You lose!";
+    document.querySelector(".reset").classList.remove("hide");
+    document.querySelector(".team-1-add-1-button").disabled = true;
+    document.querySelector(".team-1-subtract-1-button").disabled = true;
+    document.querySelector(".team-2-subtract-1-button").disabled = true;
+    document.querySelector(".team-2-add-1-button").disabled = true;
+  }
 };
 
 let subtractTeam1Score = () => {
   //function to subtract 1 to Team 1 Score
   //retrieve the current store
-  let currentScore = document.querySelector(".team-1-score").textContent;
+  let currentScore1 = document.querySelector(".team-1-score").textContent;
   //subtract 1
-  currentScore = parseInt(currentScore) - 1;
+  currentScore1 = parseInt(currentScore1) - 1;
   //update the html equal it to the subtraction function
-  document.querySelector(".team-1-score").textContent = currentScore;
-  console.log("subtracting one from team one score", currentScore);
+  document.querySelector(".team-1-score").textContent = currentScore1;
+  console.log("subtracting one from team one score", currentScore1);
+
+  if (currentScore1 < 1) {
+    console.log("Team 1 loses!");
+    outputMessageTeam1.textContent = "You lose!";
+    outputMessageTeam2.textContent = "You win!";
+    document.querySelector(".reset").classList.remove("hide");
+    document.querySelector(".team-1-add-1-button").disabled = true;
+    document.querySelector(".team-1-subtract-1-button").disabled = true;
+    document.querySelector(".team-2-subtract-1-button").disabled = true;
+    document.querySelector(".team-2-add-1-button").disabled = true;
+  }
+};
+
+//Team 2
+
+let addTeam2Score = () => {
+  let currentScore2 = document.querySelector(".team-2-score").textContent;
+  currentScore2 = parseInt(currentScore2) + 1;
+  document.querySelector(".team-2-score").textContent = currentScore2;
+  console.log("adding one to team two score", currentScore2);
+
+  if (currentScore2 >= 21) {
+    console.log("Team 2 Wins!");
+    outputMessageTeam2.textContent = "You win!";
+    outputMessageTeam1.textContent = "You lose!";
+    document.querySelector(".reset").classList.remove("hide");
+    document.querySelector(".team-1-add-1-button").disabled = true;
+    document.querySelector(".team-1-subtract-1-button").disabled = true;
+    document.querySelector(".team-2-subtract-1-button").disabled = true;
+    document.querySelector(".team-2-add-1-button").disabled = true;
+  }
 };
 
 let subtractTeam2Score = () => {
-  let currentScore = document.querySelector(".team-2-score").textContent;
-  currentScore = parseInt(currentScore) - 1;
-  document.querySelector(".team-2-score").textContent = currentScore;
-  console.log("subtracting one from team two score", currentScore);
+  let currentScore2 = document.querySelector(".team-2-score").textContent;
+  currentScore2 = parseInt(currentScore2) - 1;
+  document.querySelector(".team-2-score").textContent = currentScore2;
+  console.log("subtracting one from team two score", currentScore2);
+
+  if (currentScore2 < 1) {
+    console.log("Team 2 loses!");
+    outputMessageTeam2.textContent = "You lose!";
+    outputMessageTeam1.textContent = "You win!";
+    document.querySelector(".reset").classList.remove("hide");
+    document.querySelector(".team-1-add-1-button").disabled = true;
+    document.querySelector(".team-1-subtract-1-button").disabled = true;
+    document.querySelector(".team-2-subtract-1-button").disabled = true;
+    document.querySelector(".team-2-add-1-button").disabled = true;
+  }
 };
 
 let updateTeam1Name = () => {
@@ -71,11 +129,17 @@ document
   .addEventListener("click", subtractTeam1Score);
 
 document
+  .querySelector(".team-2-subtract-1-button")
+  .addEventListener("click", subtractTeam2Score);
+
+document
   .querySelector(".update-team-1-name")
   .addEventListener("click", updateTeam1Name);
 
 document
   .querySelector(".update-team-2-name")
   .addEventListener("click", updateTeam2Name);
+
+document.querySelector(".reset").addEventListener("click", main);
 
 document.addEventListener("DOMContentLoaded", main);
